f47588898559f3209581708f82549bd3258a32e9
Ezequiel Sosa <ezequieljsosa@gmail.com>
2020-09-03 05:15:24 -0300

    .

--- server.py
+++ server.py
@@ -11,4 +11,4 @@ def index():
     else:
         return "Hola que tal"
 if __name__ == "__main__":
-    run(host='localhost', port=8080, debug=True)
+    run(host='0.0.0.0', port=80, debug=True,reloader=True)
